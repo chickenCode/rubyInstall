85d5d62f26de6b8ca26a87720fb3eab6bb220aef
Matt Permenter <codekidd@gmail.com>
2014-06-11 17:00:18 -0400

    Added extra dependencies for rails to make easier if user wants to install rails as well

--- rubyInstall.sh
+++ rubyInstall.sh
@@ -1,7 +1,6 @@
-
 sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core \
-zlib1g zlib1g-dev libssl-dev \
-libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison -y;
+zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev \
+libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison -y
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile;
 source ~/.bash_profile;
 \curl -L https://get.rvm.io | bash -s stable
