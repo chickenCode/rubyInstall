72f41324c9b05b81202c9ecd93990ef5020691f3
Matt Permenter <codekidd@gmail.com>
2014-06-11 16:53:01 -0400

    script upload

---
+++ rubyInstall.sh
@@ -0,0 +1,14 @@
+
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core \
+zlib1g zlib1g-dev libssl-dev \
+libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison -y;
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile;
+source ~/.bash_profile;
+\curl -L https://get.rvm.io | bash -s stable
+update-alternatives --set ruby /usr/bin/ruby2.1;
+ruby -v;
+
+
+
+
+
